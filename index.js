5ce2e27bb1f4618c9085b0f4606171649d3cf641
David Perez <davperez89@gmail.com>
2017-12-10 05:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction() {
   function thefunk() {
-    var funkyFunction = 'Funky!'
-  }
+    return function funkyFunction()
 }
 
